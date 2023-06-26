8800611dec13ffb1f2789b25a124ae355267db8b
pineapplentt <jvsalss@gmail.com>
2023-06-26 10:37:15 -0300

    CardHolder creation with business rules applied

--- data/ddl.sql
+++ data/ddl.sql
@@ -20,6 +20,7 @@ CREATE TABLE IF NOT EXISTS BANK_ACCOUNT(
 
 CREATE TABLE IF NOT EXISTS CARD_HOLDER(
         card_holder_id uuid NOT NULL,
+        client_id uuid NOT NULL,
         credit_analysis_id uuid NOT NULL,
         bank_account_fk_id uuid NOT NULL,
         card_holder_status varchar(8) NOT NULL,
--- src/main/java/com/jazztech/repository/CardHolderRepository.java
+++ src/main/java/com/jazztech/repository/CardHolderRepository.java
@@ -1,8 +1,11 @@
 package com.jazztech.repository;
 
 import com.jazztech.repository.entity.CardHolderEntity;
+import com.jazztech.utils.CardHolderStatus;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardHolderRepository extends JpaRepository<CardHolderEntity, UUID> {
+    List<CardHolderEntity> findByStatus(CardHolderStatus status);
 }
